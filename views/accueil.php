ffe0e0b1008423320903f41f99fa22d411440c4c
Daouda Timera <timeradaouda5@gmail.com>
2026-03-28 22:25:03 +0100

    mise en page et forme

--- views/accueil.php
+++ views/accueil.php
@@ -17,30 +17,40 @@
       <link rel="stylesheet" href="assets/carte.css">
 </head>
 <body>
+ 
+ 
+<div id="header">
+<h1>Bienvenue sur VilleMap 🗺️</h1>
+</div>
 
-<div id="entete"> 
-<h1>Bienvenue sur VilleMap</h1>
-<h2>🔍 Recherche d'Adresses</h2>
+<div id="contenu">
+
+<div id="entete">
+<h2>🔍 Recherche d'adresses</h2>
         <form>
+
+          <input type="text" id="search" placeholder="Taper un nom de ville..."/>   
+        <div id="options">
         <select id="mode">
             <option value="commence">Commence par</option>
             <option value="contient">Contient</option>
             <option value="termine">Finit par</option>
         </select>
+        
+            <button type="button" onclick="rechercher()">🔍 Rechercher</button>
+        </div>
+       
 
-        <input type="text" id="search" placeholder="ex: plou, heim, ac..."/>
-
-        <button type="button" onclick="rechercher()">🔍 Rechercher</button>
+        
     </form>
 
     <button type="button" onclick="rechercheRapide('commence', 'K')">Commence par K</button>
-    <button type="button" onclick="rechercheRapide('termine', 'heim')">Finit par heim</button>
+    <button type="button" onclick="rechercheRapide('termine', 'orge')">Finit par orge</button>
     <button type="button" onclick="rechercheRapide('termine', 'ac')">Finit par ac</button>
-
+    </div>
+    <div id="map"></div> 
  </div>
-
-
-<div id="map"></div> 
+</div>
 
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
